ce5b2585ac2645576325e75c8f6a0aa7879901ec
Guli-Alimova <86463317+Guli-Alimova@users.noreply.github.com>
2022-07-26 04:12:50 +0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,56 @@
+
+<template>
+<div class="py-[30px] bg-black w-[640px]">
+    <div class="container px-[20px]">
+        <div>
+        <a href="/" class="flex items-center font-bold text-[22px]">
+        <img src="../src/assets/img/logowhite.png" class="w-[70px]" alt="">
+        <span class="text-[#fff] inline">ENDER</span>
+        </a>   
+        <h1 class="font-bold text-white text-[38px] pt-[30px] pb-[20px] w-[467px]">Congratulations! You've won the Competition!</h1>
+        <div class="flex justify-between">
+            <img src="../src/assets/img/img1.jpeg" class="w-[180px]" alt="rasm">
+            <img src="../src/assets/img/img2.jpeg" class="w-[180px]" alt="rasm">
+            <img src="../src/assets/img/img3.jpeg" class="w-[180px]" alt="rasm">
+        </div>
+        <p class="text-[20px] text-[#505050] w-[541px] pt-[50px] pb-[30px]">Hey, Erik! Your hard work has paid off. We've been reviewing the submissions for 
+        <span class="text-[18px] text-[#03F747]">Recreate your Room in Minecraft</span> 
+        competition and hands down your project is the best</p>
+        
+        <div class="px-[12px] py-[12px] flex justify-between border-2 rounded-sm bg-[#1B1B1B] border-[#505050] items-center">
+            <div >
+                <span class="text-[20px] text-[#505050]">You won</span>
+                <h4 class="text-[28px] font-bold text-[#fff]">+475.00</h4>
+                <span class="text-[20px] text-[#505050]">Account Balance:$175</span>
+            </div>
+                <button class="bg-white px-[25px] py-[5px] text-black h-[35px] rounded-[3px] font-bold">VIEW RESULTS</button>
+        </div>
+
+        <div class="pt-[35px] pb-[50px] border-t-gray-200">
+            <div class="flex items-center ">
+            <span class="text-[36px] text-white font-bold pr-[10px] pt-[10px]">"</span>
+            <span class="text-[#505050]">Elon Mask, CEO of SpaceX</span>
+            </div>
+            
+            <p class="text-[20px] text-white font-bold">We loved the blend between Pokemon and Minecraft and the pixel art looks really good. Surprisingly, this was Erik's first Pixel Art project and it turned out great.</p>
+        </div>
+
+        
+        
+        </div>
+       
+    </div>
+
+</div>
+
+
+</template>
+
+<script setup>
+
+</script>
+
+
+<style scoped>
+
+</style>
