ed208a3a8ea2ce93360a6f47f31dc15b86fbb22f
Guli-Alimova <86463317+Guli-Alimova@users.noreply.github.com>
2022-07-26 04:32:47 +0500

    addtion

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 
 <template>
-<div class="py-[30px] bg-black w-[640px]">
+<div class="py-[30px] bg-black w-[640px] my-0 mx-auto">
     <div class="container px-[20px]">
         <div>
         <a href="/" class="flex items-center font-bold text-[22px]">
@@ -26,7 +26,7 @@
                 <button class="bg-white px-[25px] py-[5px] text-black h-[35px] rounded-[3px] font-bold">VIEW RESULTS</button>
         </div>
 
-        <div class="pt-[35px] pb-[50px] border-t-gray-200">
+        <div class="pt-[35px] pb-[50px] border-b-2 border-[#505050]">
             <div class="flex items-center ">
             <span class="text-[36px] text-white font-bold pr-[10px] pt-[10px]">"</span>
             <span class="text-[#505050]">Elon Mask, CEO of SpaceX</span>
@@ -35,7 +35,13 @@
             <p class="text-[20px] text-white font-bold">We loved the blend between Pokemon and Minecraft and the pixel art looks really good. Surprisingly, this was Erik's first Pixel Art project and it turned out great.</p>
         </div>
 
+        <div class="flex py-[20px]">
+            <img  class="w-[30px]" src="../src/assets/img/cube-svgrepo-com.svg" alt="">
+            <span class="text-[#505050] text-[20px] font-bold pl-2">ENDER</span>
+        </div>
+        <span class="text-[#505050] text-[16px] border-b-2">Joinder.com,</span><span class="text-[#505050] text-[16px]">short platform description text here.</span>
         
+        <p class="text-[#505050] text-[16px] py-3">Messages from Ender will always come from a <span class="text-[#505050] text-[16px] border-b-2">@joinder.com </span>email address. we will never ask you for account information like your password or answers to security quastions</p>
         
         </div>
        
